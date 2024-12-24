48adde7c37bde5bd2433b91644b7f889d0ea6dc9
copycode69 <jaytiwari092@gmail.com>
2024-12-24 21:38:15 +0530

    update

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def result(filename):
     return render_template('result.html', filename=filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
